6b2219abb7d889d3b0d7dd5233cc4a4c72f96dfb
Davi mota <davi.mota68@outlook.com>
2026-01-22 23:40:05 +0000

    22-01-26 correção e finalização nivel novato

--- If_SuperTrunfo.c
+++ If_SuperTrunfo.c
@@ -6,17 +6,17 @@ int main(){
     char estado1[3], estado2[3];
     char codigocarta1[3], codigocarta2[3], cidade1[50], cidade2[50];
     unsigned int populacao1, populacao2;
-    unsigned short pontosturisticos1, pontosturisticos2;
+    short pontosturisticos1, pontosturisticos2;
     float area1, area2, densidadepop1, densidadepop2;
     double pib1, pib2, pibpercapita1, pibpercapita2;
 
     printf(".:::.=================>Carta1<=================.:::.\n");
     // Recepção de dados para carta 1
     printf("\nEstado: ");
-    scanf("%2s", estado1);
+    scanf(" %2s", estado1);
 
     printf("Código da carta: ");
-    scanf("%2s", codigocarta1);
+    scanf(" %2s", codigocarta1);
     
     printf("Cidade: ");
     getchar();
@@ -33,19 +33,19 @@ int main(){
     scanf("%lf", &pib1);
 
     printf("Número de pontos turísticos: ");
-    scanf("%hs", &pontosturisticos1);
+    scanf(" %hd", &pontosturisticos1);
 
     printf(".:::.=================>Carta2<=================.:::.\n");
     // Recepção de dados para carta 2
     printf("\nEstado: ");
-    scanf("%2s", estado2);
+    scanf(" %2s", estado2);
 
     printf("Código da carta: ");
-    scanf("%2s", codigocarta2);
+    scanf(" %2s", codigocarta2);
     
     printf("Cidade: ");
     getchar(); // consome o '\n' deixado pelo scanf
-    fgets(cidade2, sizeof(cidade2), stdin); // usado no lugar de scanf para textos, nomes com mais de uma palavra
+    fgets(cidade2, sizeof(cidade2), stdin); // usado no lugar de scanf para receber textos, nomes com mais de uma palavra
     cidade2[strcspn(cidade2, "\n")] = '\0'; // tira o \n do fgets
 
     printf("População: ");
@@ -58,21 +58,21 @@ int main(){
     scanf("%lf", &pib2);
 
     printf("Número de pontos turísticos: ");
-    scanf("%hs", &pontosturisticos2);
+    scanf(" %hd", &pontosturisticos2);
 
     printf("\n.:::.=================>Super Trunfo Carta1<=================.:::.\n");
     // Iformações e calculos de dados da carta 1
     densidadepop1 = populacao1 / area1;
     pibpercapita1 = pib1 / populacao1;
-    printf("%.2f\n", densidadepop1);
-    printf("%.2f\n", pibpercapita1);
+    printf("Densidade Populacional: %.2f\n", densidadepop1);
+    printf("PIB per Capita: R$ %.2f\n", pibpercapita1);
 
     printf("\n.:::.=================>Super Trunfo Carta2<=================.:::.\n");
     // Iformações e calculos de dados da carta 2
     densidadepop2 = populacao2 / area2;
     pibpercapita2 = pib2 / populacao2;
-    printf("%.2f\n", densidadepop2);
-    printf("%.2f\n", pibpercapita2);
+    printf("Densidade Poulacional: %.2f\n", densidadepop2);
+    printf("PIB per Capita: R$ %.2f\n", pibpercapita2);
 
     //Escolha de carta vencedora com base no atributo maior entre carta 1 e carta 2.
     printf("Carta 1 - %s (%s): %d\n", cidade1, estado1, populacao1);
